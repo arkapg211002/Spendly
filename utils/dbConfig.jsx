bb1ba6569e44f39f17334db6c7430c81e57288b2
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-06-29 15:33:36 +0530

    Update dbConfig.jsx

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
-const sql = neon('postgresql://neondb_owner:G0oSDfv2mVTB@ep-dark-firefly-a1q3jw5f.ap-southeast-1.aws.neon.tech/Spendly?sslmode=require');
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!);
 export const db = drizzle(sql, { schema });
